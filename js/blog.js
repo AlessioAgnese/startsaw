6376952dde9e6372d179fbf2ae73578ff4979776
AlessioAgnese <alessio.agnese@outlook.it>
2019-05-25 18:05:04 +0200

    Estratti tutti sul database

--- js/blog.js
+++ js/blog.js
@@ -1,1 +1,26 @@
 //WORKIN PROGRESS
+$(document).ready(function() {
+    $.ajax({
+                url: 'http://localhost/php/getblog.php',
+                type: 'POST',
+                dataType: 'json',
+                data: JSON.stringify({}),
+                success: function(data) {
+                    if (data.ok) {
+                        $.each(data.rows, function(index){
+                        console.log(data.rows[index].Testo);
+                        console.log(data.rows[index].User);
+                        console.log(data.rows[index].Id_A);
+                        console.log(data.rows[index].Data);});
+                        //alert("pubblicato?");
+                        //$('#parent').append('<div>hello</div>');    
+                        //$windows.body.append( data.rows[0].Data );
+                    } else {
+                        alert("errore nella pubblicazione");
+                    }
+                },
+                error: function(errorThrown) {
+                    console.log(errorThrown);
+                }
+            })
+        })
--- php/getblog.php
+++ php/getblog.php
@@ -1,12 +1,12 @@
 <?php
 include_once('dbconfig.php');
 global $conn;
-$select = $conn->prepare("SELECT * FROM Articoli ORDER BY DESC");
+$select = $conn->prepare("SELECT * FROM Articoli ORDER BY Data DESC");
 $select->execute();
 if($select){
-    $article = $select->fetch(PDO::FETCH_ASSOC);
-    $array =array("ok"=>true);
-            echo json_encode($array);       
+    $rows = $select->fetchAll(PDO::FETCH_ASSOC);
+    $array=array("ok"=>true,"rows"=>$rows); 
+    echo json_encode($array);       
 }else{
     $array=array("ok"=>false);
     echo json_encode($array);}    
