8955f23e706f2d2e8a64fef6cc75fee5994f05a6
Alessio <alessio.agnese@outlook.it>
2019-05-26 21:56:55 +0200

    Leggeri cambiamenti

--- js/printer.js
+++ js/printer.js
@@ -1,9 +1,38 @@
-tinymce.init({
-    selector: 'textarea',
-    language_url: './tinymce/it_IT.js',
-    language: 'it_IT',
 
-     });
+$(document).ready(function(){
+    if('token' in localStorage){
+        
+            $.ajax({
+                url:'http://localhost/php/checklogin.php',
+                type:'post',
+                dataType:'json',
+                data:JSON.stringify({
+                    token:localStorage.getItem('token')
+                }),
+                success:function(data){
+                    if(data.ok && data.utente != false && data.perm==3){
+                        tinymce.init({
+                            selector: 'textarea',
+                            language_url: './js/it_IT.js',
+                            language: 'it_IT',});
+                    $('#articolo').css("visibility", "visible");
+                    $('#publish').css("visibility", "visible");        
+                }
+                    else{
+                        alert("Non hai i permessi per accedere a questa pagina");
+                        window.location.replace("http://localhost");
+                    }
+                },
+                error:function(errorThrown){
+                    console.log(errorThrown);
+                }
+            })
+        }else{
+            alert("Devi effetuare il login");
+            window.location.replace("http://localhost");
+        }
+    })
+
 $(document).ready(function() {
         $('#publish').click(function() {
             tmp=tinymce.get('articolo').getContent().toString().length;
