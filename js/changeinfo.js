6326569784e0302b88dbaa2eed7dc3389c4ae7de
AlessioAgnese <alessio.agnese@outlook.it>
2019-05-30 13:21:13 +0200

    gimmics

--- js/changeinfo.js
+++ js/changeinfo.js
@@ -1,77 +1,86 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    document.querySelector('#fileI').addEventListener('change', function(e) {
+    document.querySelector('#fileI').addEventListener('change', function (e) {
         var file = this.files[0];
-        if(file.size>16777215){alert("file troppo grosso"); return false;}
-        checkMIME(file).then(x=>{
+        if (file.size > 16777215) {
+            alert("file troppo grosso");
+            return false;
+        }
+        checkMIME(file).then(x => {
             var fd = new FormData();
             fd.append("fileI", file);
             fd.append("token", localStorage.getItem('token'));
             var xhr = new XMLHttpRequest();
             xhr.open('POST', './php/manageImg.php', true);
             xhr.send(fd);
-            xhr.upload.onprogress = function(e) {
-            var percentComplete=0;
-            while(e.lengthComputable <99){  
-              if (e.lengthComputable) {
-                percentComplete = (e.loaded / e.total) * 100;
-              }
-              if(percentComplete == 100){location.reload(); break;}
-              else{
-                $('#profilePic').addClass('is-loading');
-              } 
+            xhr.upload.onprogress = function (e) {
+                var percentComplete = 0;
+                while (e.lengthComputable < 99) {
+                    if (e.lengthComputable) percentComplete = (e.loaded / e.total) * 100;
+                    if (percentComplete == 100) {break;}
+                }
+                $("#notif").removeClass("is-danger").addClass("is-link");
+                $("#notif").css("display", "block");
+                $("#notifText").text("Immagine modificata con successo");
             }
-            }
-        }).catch(y=>{
-            alert("Inserisci una immagine");
-            return false;}) 
-      }, false);
+        }).catch(y => {
+            $("#notif").removeClass("is-link").addClass("is-danger");
+                $("#notif").css("display", "block");
+                $("#notifText").text("Inserisci un'immagine");
+            return false;
+        })
+    }, false);
+
+
+
     //PRENDE IMG E LA METTE 
     $.ajax({
-        url:'http://localhost/php/manageImg.php',
-        type:'GET',
-        dataType:'json',
+        url: 'http://localhost/php/manageImg.php',
+        type: 'GET',
+        dataType: 'json',
         beforeSend: function (xhr) {
-            xhr.setRequestHeader('X-Authentication',localStorage.getItem('token'));
+            xhr.setRequestHeader('X-Authentication', localStorage.getItem('token'));
         },
-        success:function(data){
-            if(data.ok && data.dataUrl != null){
-                $('#profilePic').attr("src",data.dataUrl);
-            }else{
-                alert("Errore nel recupero delle immagini");
-                }
+        success: function (data) {
+            if (data.ok && data.dataUrl != null) {
+                $('#profilePic').attr("src", data.dataUrl);
+
+            } else {
+                $("#notif").removeClass("is-link").addClass("is-danger");
+                $("#notif").css("display", "block");
+                $("#notifText").text("Errore nel recupero dell'immagine,riprova più tardi");
+            }
         },
-        error:function(errorThrown){
+        error: function (errorThrown) {
             console.log(errorThrown);
         }
     })
 
 
-    if('token' in localStorage){
+    if ('token' in localStorage) {
         $.ajax({
-            url:'http://localhost/php/userinfo.php',
-            type:'get',
-            dataType:'json',
+            url: 'http://localhost/php/userinfo.php',
+            type: 'get',
+            dataType: 'json',
             beforeSend: function (xhr) {
                 xhr.setRequestHeader('X-Type', 'view');
-                xhr.setRequestHeader('X-Authentication',localStorage.getItem('token'));
+                xhr.setRequestHeader('X-Authentication', localStorage.getItem('token'));
 
             },
-            success:function(data){
-                if(data.ok){
+            success: function (data) {
+                if (data.ok) {
                     $('#nome').val(data.nome);
                     $('#cognome').val(data.cognome);
                     $('#residenza').val(data.residenza);
                     $('#biografia').val(data.biografia);
-                }
-                else{
+                } else {
                     alert("La sessione è scaduta");
 
-                    localStorage.removeItem('token'); 
-                    
-                    }
+                    localStorage.removeItem('token');
+
+                }
             },
-            error:function(errorThrown){
+            error: function (errorThrown) {
                 console.log(errorThrown);
             }
         })
@@ -79,18 +88,20 @@ $(document).ready(function(){
 
     $('#fileD').click(function () {
         $.ajax({
-            url:'http://localhost/php/userinfo.php',
-            type:'get',
-            dataType:'json',
+            url: 'http://localhost/php/userinfo.php',
+            type: 'get',
+            dataType: 'json',
             beforeSend: function (xhr) {
                 xhr.setRequestHeader('X-Type', 'delete');
-                xhr.setRequestHeader('X-Authentication',localStorage.getItem('token'));
+                xhr.setRequestHeader('X-Authentication', localStorage.getItem('token'));
             },
             success: function (data) {
                 if (data.ok) {
-                    window.location.reload();
+                $("#notif").removeClass("is-danger").addClass("is-link");
+                $("#notif").css("display", "block");
+                $("#notifText").text("Immagine Cancellata");
                 } else {
-                   alert("Cancellazione finita male");
+                    alert("Cancellazione finita male");
                 }
             },
             error: function (errorThrown) {
@@ -101,29 +112,29 @@ $(document).ready(function(){
 
     $('#submitChange').click(function () {
         $.ajax({
-            url:'http://localhost/php/userinfo.php',
-            type:'post',
-            dataType:'json',
+            url: 'http://localhost/php/userinfo.php',
+            type: 'post',
+            dataType: 'json',
             beforeSend: function (xhr) {
                 xhr.setRequestHeader('X-Type', 'edit');
             },
-            data:JSON.stringify({
-                token:localStorage.getItem('token'),
-                nome:$('#nome').val().trim().toString(),
-                cognome:$('#cognome').val().trim().toString(),
-                residenza:$('#residenza').val().trim().toString(),
-                biografia:$('#biografia').val().trim().toString(),
+            data: JSON.stringify({
+                token: localStorage.getItem('token'),
+                nome: $('#nome').val().trim().toString(),
+                cognome: $('#cognome').val().trim().toString(),
+                residenza: $('#residenza').val().trim().toString(),
+                biografia: $('#biografia').val().trim().toString(),
             }),
             success: function (data) {
                 if (data.ok) {
                     $("#notif").removeClass("is-danger").addClass("is-link");
-                    $("#notif").css("display","block");
+                    $("#notif").css("display", "block");
                     $("#notifText").text("Modifiche effettuate con successo");
-                   
+
 
                 } else {
                     $("#notif").removeClass("is-link").addClass("is-danger");
-                    $("#notif").css("display","block");
+                    $("#notif").css("display", "block");
                     $("#notifText").text("Qualcosa è andato storto, ricontrolla i campi");
                 }
             },
@@ -137,25 +148,25 @@ $(document).ready(function(){
 
     $('#ChangePdw').click(function () {
         $.ajax({
-            url:'http://localhost/php/userinfo.php',
-            type:'post',
-            dataType:'json',
+            url: 'http://localhost/php/userinfo.php',
+            type: 'post',
+            dataType: 'json',
             beforeSend: function (xhr) {
                 xhr.setRequestHeader('X-Type', 'change');
             },
-            data:JSON.stringify({
-                token:localStorage.getItem('token'),
-                oldPwd:$('#oldPwd').val().toString(),
-                newPwd:$('#pwdR').val().toString(),
+            data: JSON.stringify({
+                token: localStorage.getItem('token'),
+                oldPwd: $('#oldPwd').val().toString(),
+                newPwd: $('#pwdR').val().toString(),
             }),
             success: function (data) {
                 if (data.ok) {
                     $("#notif").removeClass("is-danger").addClass("is-link");
-                    $("#notif").css("display","block");
+                    $("#notif").css("display", "block");
                     $("#notifText").text("Password modificata con successo");
                 } else {
                     $("#notif").removeClass("is-link").addClass("is-danger");
-                    $("#notif").css("display","block");
+                    $("#notif").css("display", "block");
                     $("#notifText").text("Qualcosa è andato storto, ricontrolla i campi");
                 }
             },
@@ -165,24 +176,24 @@ $(document).ready(function(){
         })
     })
 });
-$('#fileI').on('change',function(e){
+$('#fileI').on('change', function (e) {
     var formdata = new FormData();
     formdata.append('image', $('input[type=file]')[0]);
     e.preventDefault();
     $.ajax({
-        url:'http://localhost/php/userinfo.php',
-        type:'POST',
-        beforeSend:function(xhr){
-            xhr.setRequestHeader('X-Type','avt');
-            xhr.setRequestHeader('X-Authentication',localStorage.getItem('token'));
+        url: 'http://localhost/php/userinfo.php',
+        type: 'POST',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('X-Type', 'avt');
+            xhr.setRequestHeader('X-Authentication', localStorage.getItem('token'));
         },
-        data:formdata,
-        cache:false,
-        processData:false,
-        success:function(data){
+        data: formdata,
+        cache: false,
+        processData: false,
+        success: function (data) {
 
         },
-        error:function(errorThrown){
+        error: function (errorThrown) {
             console.log(errorThrown);
         }
     })
