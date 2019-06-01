2d56b7c147594e6e8b4b4423ff4fe593362e205d
Alessio <alessio.agnese@outlook.it>
2019-06-01 17:01:06 +0200

    prototipo profilo

--- php/dbconfig.php
+++ php/dbconfig.php
@@ -2,12 +2,23 @@
     $dsn = 'mysql:host=localhost;dbname=blog';
     $username = 'root';
     $password = '';
-    $conn = new PDO($dsn, $username, $password);
+        
+try {
+    $conn = new PDO($dsn, $username, $password); // PDO Driver DSN. Throws A PDOException.
+}
+catch( PDOException $Exception ) {
+    throw new MyDatabaseException( $Exception->getMessage( ) , $Exception->getCode( ) );
+    /*$array = array(
+        "ok"=>false,
+        "message"=>$Exception->getMessage(),
+        "code"=>(int)$Exception->getCode());
+    echo json_encode($array);*/
+}
 /*
     Per connessione server unige(non funzionante)
     $dsn = 'mysql:host="localhost";dbname=S4396998';
     $username = 'S4396998';
     $password = '85aWs#9vp#';
     $conn = new PDO($dsn, $username, $password);
-    */
+*/
 ?>
